751fdef150ebdc175a890cc8ad1a24d16a430be6
Bryce Corbitt <bscorbitt@wpi.edu>
2019-10-09 18:36:25 -0400

    Added values for speed & direction to refer to.

--- dc_driver.py
+++ dc_driver.py
@@ -8,27 +8,32 @@ class DCDriver(object):
     self.en = en
     self.in1 = in1
     self.in2 = in2
+    self.direction = 1
+    self.speed = 0
     GPIO.setup(in1, GPIO.OUT)
     GPIO.setup(in2, GPIO.OUT)
     GPIO.setup(en, GPIO.OUT)
     GPIO.output(in1, GPIO.HIGH)
     GPIO.output(in2, GPIO.LOW)
     self.p=GPIO.PWM(en, 1000)
-    self.p.start(0)
+    self.p.start(self.speed)
 
   def set_direction(self, value):
     if value:
       GPIO.output(self.in1, GPIO.HIGH)
       GPIO.output(self.in2, GPIO.LOW)
+      self.direction = 1
     else:
       GPIO.output(self.in1, GPIO.LOW)
       GPIO.output(self.in2, GPIO.HIGH)
+      self.direction = 0
 
   def set_speed(self, value):
+    self.speed = value
     self.p.ChangeDutyCycle(value)
   
   def stop(self):
-    self.p.ChangeDutyCycle(0)
+    self.set_speed(0)
 
 # driver = DCDriver(13, 26, 16)
 # driver.set_speed(100)
